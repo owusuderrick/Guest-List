86fe99c44861204e976f8bcc8ce9cc8f86c2fe70
Derrick Owusu <owusuderek@gmail.com>
2026-05-11 00:51:49 -0700

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+
+import GuestDetails from "./guests/GuestDetails";
+import GuestList from "./guests/GuestList";
+
 export default function App() {
+  const [guestId, setGuestId] = useState(null);
+  return (
+    <main>
+      {guestId ? (
+        <GuestDetails guestId={guestId} setGuestId={setGuestId} />
+      ) : (
+        <GuestList setGuestId={setGuestId} />
+      )}
+    </main>
+  );
+}export default function App() {
   return <></>;
 }
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+const BASE_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const COHORT = "/2602-Derrick";
+export const API = BASE_URL + COHORT;
